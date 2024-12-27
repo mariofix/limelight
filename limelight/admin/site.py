52e2ffc689cfeb3993eec760b25320cd569d7fea
Mario Hernandez <mariofix@proton.me>
2024-12-27 02:13:58 -0300

    raw info

--- limelight/admin/site.py
+++ limelight/admin/site.py
@@ -3,13 +3,11 @@ from flask_admin.theme import Bootstrap4Theme  # type: ignore
 from flask_babel import lazy_gettext as _
 
 from ..database import db
-from ..models import Project, ProjectTags, Role, Tag, User
-from .views import ProjectAdmin, ProjectTagAdmin, RoleAdmin, TagAdmin, UserAdmin
+from ..models import Project, ProjectTags, Tag
+from .views import ProjectAdmin, ProjectTagAdmin, TagAdmin
 
 admin_site = Admin(name="limelight", theme=Bootstrap4Theme(swatch="pulse", fluid=False))
 
-# admin_site.add_view(UserAdmin(User, db.session, category=_("System")))
-# admin_site.add_view(RoleAdmin(Role, db.session, category=_("System")))
 admin_site.add_view(ProjectAdmin(Project, db.session, category=_("Projects")))
 admin_site.add_view(TagAdmin(Tag, db.session, category=_("Projects")))
 admin_site.add_view(ProjectTagAdmin(ProjectTags, db.session, category=_("Projects")))
--- limelight/utils.py
+++ limelight/utils.py
@@ -1,8 +1,28 @@
 import datetime
 
 import requests
+from packaging.specifiers import SpecifierSet
+from packaging.version import Version
 
 from .version import __version__
+
+
+def expand_requires_python(requires_python, max_python_version="3.13"):
+    if not requires_python:
+        raise ValueError("requires_python specifier cannot be empty.")
+
+    specifier = SpecifierSet(requires_python)
+
+    max_major, max_minor = map(int, max_python_version.split("."))
+    versions = []
+
+    for major in range(2, max_major + 1):
+        for minor in range(0, max_minor + 1):
+            version = Version(f"{major}.{minor}")
+            if version in specifier:
+                versions.append(str(version))
+
+    return versions
 
 
 def fetch_project_data(url):
@@ -19,6 +39,7 @@ def process_pypi_data(project):
         project.title = project_info.get("name", None)
         project.description = project_info.get("summary", None)
         project.project_url = project_info.get("project_url", None)
+        project.supported_python = expand_requires_python(project_info.get("requires_python"))
 
     project.fetch_date_next = project.created_at + datetime.timedelta(days=7)
     db.session.commit()
