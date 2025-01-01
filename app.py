282095f28af660c9d17f60e4e929f74705e794e8
Mario Hernandez <mariofix@proton.me>
2025-01-01 03:34:35 -0300

    sentry

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 import os
-
 from dotenv import load_dotenv
+import sentry_sdk
+
+load_dotenv()
+
+sentry_sdk.init(
+    dsn=os.getenv("FLASK_SENTRY_DSN"),
+    traces_sample_rate=1.0,
+    profiles_sample_rate=1.0,
+)
 
 from limelight import create_app
 
-load_dotenv()
 
 app_settings_file = os.getenv("FLASK_APP_SETTINGS_FILE")
 flask_app = create_app(app_settings_file)
